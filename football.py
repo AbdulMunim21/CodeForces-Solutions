ebc7d61cb6edaf8bae4f5a1454a7c092ed86d223
Abdul Munim <amunim1999@gmail.com>
2022-08-02 20:25:01 +0500

    young physicist

--- football.py
+++
@@ -1,6 +0,0 @@
-value = input()
-
-if "0000000" in value or "1111111" in value:
-    print('YES')
-else:
-    print('NO')
---
+++ young_physicist.py
@@ -0,0 +1,24 @@
+lines = eval(input())
+twoDArr = []
+answers = [0, 0, 0]
+for i in range(lines):
+    value = input()
+    seperated = value.split(" ")
+    twoDArr.append([int(seperated[0]), int(seperated[1]), int(seperated[2])])
+
+for i in range(lines):
+    for j in range(len(twoDArr[i])):
+        answers[j] += twoDArr[i][j]
+
+condition = True
+for i in answers:
+    if i != 0:
+        condition = True
+        break
+    else:
+        condition = False
+
+if condition:
+    print("NO")
+else:
+    print("YES")
