4df28ac3968da5e1e97cef05931aef16968c62fd
AbdulMunim <amunim1999@gmail.com>
2024-09-29 20:52:34 +0500

    removed

--- beautiful_year.py
+++
@@ -1,9 +0,0 @@
-year = input()
-int_year = int(year) + 1
-set_year = set(str(int_year))
-
-while(len(set_year) != 4): 
-    int_year = int_year + 1
-    set_year = set(str(int_year))
-    
-print(int_year)
--- in_search_of_easy_problem.py
+++
@@ -1,8 +0,0 @@
-candidates = input()
-opinions = input()
-opinions = opinions.split(" ")
-
-if "1" in opinions:
-    print("HARD")
-else:
-    print("EASY")
